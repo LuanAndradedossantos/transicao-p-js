096f21c0e74e58768f40544d0729439c7fa884d0
luan andrade dos santos <luan.andrade2207@gmail.com>
2026-06-22 20:15:49 -0300

    condições compostas
    estou adicionando condição composta mais ainda não terminei.

--- calcu-pin.js
+++ calcu-pin.js
@@ -3,6 +3,12 @@ function opera()
 
     let suvinil = window.document.getElementById("marca1");
     let coral = window.document.getElementById("marca2");
+
+    let demao1 = document.querySelector('input#demao1');
+    let demao2 = document.querySelector('input#demao2');
+    let demao3 = document.querySelector('input#demao3');
+    let demao4 = document.querySelector('input#demao4');
+    let demao5 = document.querySelector('input#demao5');
     
 
     /*transformando o texto em numero para efetuar o calculo*/
@@ -11,19 +17,25 @@ function opera()
     let area = larg * alt;
     let tinta = area / 10;
     let tinta1 = area / 6.666;
+    let tinta2 = area / 5;
     /*a variavel abaixo foi criada e deixada vazia para receber a variavel tinta ou tinta 1 dependendo da escolha no input radio. ela será usada nas condiçoes if e else*/
     let tintafinal = 0;
+    let res = document.querySelector('#result');
+    let textodemao = "";
 
 /*Para criar uma condição if/else baseada em um input do tipo radio, é preciso marcar o elemento usando a propriedade  '.checked'*/
-    if (suvinil.checked) {
-        tintafinal = tinta;
+    if (suvinil.checked && demao2.checked) {
+        tintafinal = tinta2;
+        textodemao = "duas(2) demãos de tinta se a parede já estiver pintada ou conter massa corrida";
+
     } else {
-        tintafinal = tinta1;}
+        tintafinal = tinta1;
+        textodemao = "apenas uma demão de tinta se a parede já estiver pintada ou conter massa corrida";}
     /*aqui o innerhtml adiciona um texto com o resultado do calculo que é feito quando a função é disparada atravez do click do butão.
     O .toFixed() serve para delimitar a quantidade de numeros que aparecem nos resultados colidos que ficam nas variaveis.
     
     antes de imprimir texto atravez do innerhtml é necessario resgatar a tag atravez do get elementbyid ou selctorquire para poder manipular com inner html*/
-    window.document.getElementById("result").innerHTML = `Sua parede tem a dimensão de ${larg} x ${alt} e sua area é de ${area}m&sup2;, voce   
-    precisará de ${tintafinal.toFixed(3)}L de tinta para pintar essa parede com apenas uma demão de tinta se a parede ja estiver pintada ou conter massa corrida,`
+    res.innerHTML = `Sua parede tem a dimensão de ${larg} x ${alt} e sua area é de ${area}m&sup2;. ` + `voce   
+    precisará de ${tintafinal.toFixed(3)}L de tinta para pintar essa parede com  ${textodemao},`;
 
 }
